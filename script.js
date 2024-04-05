6ac1b36bf98327fbf36b992c7b1cb8eaaf3c17ae
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-05 17:41:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let count = 0;
+	s1=s1.toLowerCase();
+	s2=s2.toLowerCase();
+	for(let i =0; i < s1.length;i++){
+		for(let j = 0; s2.length;j++){
+			if(s1[i]===s2[j]){
+				count++:
+			}
+		}
+	}
+	return count-1;
 }
 
 // Please do not change the code below
